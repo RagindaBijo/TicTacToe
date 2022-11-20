f06322f105803286ed79e289b25fe89d628f62ed
Luka <luka.ivaniadze.2@btu.edu.ge>
2022-11-20 04:20:30 +0400

    First commit

---
+++ app/src/main/java/com/example/shualeduridaigi/LastActivity.kt
@@ -0,0 +1,58 @@
+package com.example.shualeduridaigi
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class LastActivity : AppCompatActivity() {
+
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_last)
+
+        val resultWinner=intent.extras?.getString("winnerPlayer")
+        val resultFirst=intent.extras?.getString("firstPlayer")
+        val resultSecond=intent.extras?.getString("secondPlayer")
+        val winningNum=intent.extras?.getInt("winningScore")
+        var resultNum=intent.extras?.getInt("winnerNum")
+
+        var btn1=findViewById<Button>(R.id.reMatch)
+        var btn2=findViewById<Button>(R.id.newGame)
+        var text=findViewById<TextView>(R.id.winnerText)
+
+        if (resultNum==1) {
+            text.setTextColor(Color.RED)
+            text.text="Winner Is ${resultWinner}"
+        }else if(resultNum==2){
+            text.setTextColor(Color.GREEN)
+            text.text="Winner Is ${resultWinner}"
+        }
+
+
+        btn1.setOnClickListener {
+            var rematchNum=1
+            val intent1= Intent(this,first::class.java).also {
+                it.putExtra("firstPlayer", resultFirst)
+                it.putExtra("secondPlayer", resultSecond)
+                it.putExtra("rematch",rematchNum)
+                it.putExtra("winningScore",winningNum)
+            }
+            startActivity(intent1)
+        }
+
+        btn2.setOnClickListener {
+            val intent2= Intent(this,MainActivity::class.java)
+            startActivity(intent2)
+        }
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/shualeduridaigi/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.shualeduridaigi
+
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        val text1=findViewById<EditText>(R.id.editText1)
+        val text2=findViewById<EditText>(R.id.editText2)
+        val text3=findViewById<EditText>(R.id.editText3)
+        val myButton=findViewById<Button>(R.id.myButton)
+
+
+        myButton.setOnClickListener {
+
+            val resultText1= text1.text.toString()
+            val resultText2 = text2.text.toString()
+            val resultText3=text3.text.toString().toInt()
+
+            if(resultText1 == "" || resultText2=="") {
+                Toast.makeText(this,"გრაფა ცარიელია",Toast.LENGTH_SHORT).show()
+            }else{
+                val intent = Intent(this, first::class.java).also {
+                    it.putExtra("firstPlayer", resultText1)
+                    it.putExtra("secondPlayer", resultText2)
+                    it.putExtra("winningScore", resultText3)
+
+                }
+                startActivity(intent)
+            }
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
